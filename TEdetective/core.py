d6d8a4efe79d3337d4c8b123fe9ae2070d79f08e
jredwards417 <jredwards@wustl.edu>
2022-06-21 08:33:54 -0500

    fix subparser error

--- TEdetective/core.py
+++ TEdetective/core.py
@@ -2510,7 +2510,9 @@ def main():
             }
 
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest='command', required=True)
+    #subparsers = parser.add_subparsers(dest='command', required=True)
+    subparsers = parser.add_subparsers(dest='command')
+    subparsers.required = True
 
     sp_preprocess = subparsers.add_parser('preprocess', help="preprocess argument")
     sp_preprocess.add_argument('-bam', action='store', dest='bam_inp', required=True, help='Bam(.bam) file with full path')

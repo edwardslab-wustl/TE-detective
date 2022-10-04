56a39f072568aeea8e1fa52617c2602f49e10235
jredwards417 <jredwards@wustl.edu>
2022-10-04 16:53:58 -0500

    cleaned up example_data

--- example_data/clean.sh
+++
@@ -1,7 +0,0 @@
-# quick shell script to clean up intermediate and final results to rerun example data
-rm -rf intermediate_files
-rm -rf preprocessed_files
-rm final_results
-rm recluster*
-rm initial_pred*
-
--- example_data/run_example.sh
+++
@@ -1,7 +0,0 @@
-echo "PREPROCESS" && TE_detective preprocess -i test_sim.bam -r ref_fofn \
-    && echo 'DISCOVER' && TE_detective discover -i test_sim.bam -r ref_fofn \
-    && echo 'NADISCOVER' && TE_detective nadiscover -i test_sim.bam -r ref_fofn --polyA --discord_cluster_dens 5 \
-    && echo 'ANALYZE' && TE_detective analyze -i test_sim.bam -r ref_fofn --inp initial_predictions.txt \
-    && echo 'CLUSTER2D' && TE_detective cluster2d -i test_sim.bam -r ref_fofn --discord_cluster_dens 5 --all \
-    && echo 'FILTER' && TE_detective filter -i final_results.tsv --bed rmsk_ucsc_mm10.bed \
-    && echo 'FILTER_NORMAL' && TE_detective filter -i final_results.tsv --filter normal --ini_filter normal --bed rmsk_ucsc_mm10.bed -o filter_normal_results.txt\
